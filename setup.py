09468e52930cebe7f7622533bc4b9036913b09ad
cutz <christopher.m.utz@gmail.com>
2021-06-02 14:24:56 -0500

    initial shell

---
+++ setup.py
@@ -0,0 +1,68 @@
+import codecs
+from setuptools import setup
+from setuptools import find_packages
+
+entry_points = {
+    'console_scripts': [
+    ],
+    "z3c.autoinclude.plugin": [
+        'target = nti.app',
+    ],
+}
+
+TESTS_REQUIRE = [
+]
+
+
+def _read(fname):
+    with codecs.open(fname, encoding='utf-8') as f:
+        return f.read()
+
+
+setup(
+    name='nti.app.products.xr',
+    version='0.0.1.dev0',
+    author='Chris Utz',
+    author_email='chris.utz@nextthought.com',
+    description="NTI XR Content",
+    long_description=(
+        _read('README.rst')
+        + '\n\n'
+        + _read("CHANGES.rst")
+    ),
+    license='Apache',
+    keywords='Products xr',
+    classifiers=[
+        'Framework :: Zope3',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    url="https://github.com/NextThought/nti.app.products.xr",
+    zip_safe=False,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    namespace_packages=['nti', 'nti.app', 'nti.app.products'],
+    tests_require=TESTS_REQUIRE,
+    install_requires=[
+        'setuptools'
+    ],
+    extras_require={
+        'test': TESTS_REQUIRE,
+        'docs': [
+            'Sphinx',
+            'repoze.sphinx.autointerface',
+            'sphinx_rtd_theme',
+        ],
+    },
+    entry_points=entry_points,
+)
